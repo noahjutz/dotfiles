2e3327c1182d796653a2100788e67bd4c851f070
noahjutz <noah.jutz@gmail.com>
2020-05-17 00:16:49 +0200

    Add htop to auto-install

--- .scripts/auto-install.sh
+++ .scripts/auto-install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo pacman -Syy
-sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher
+sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher htop
 yay -S android-studio bitwarden
