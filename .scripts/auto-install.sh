e80554aac97268accbc3b7b44900e4abe66d5240
noahjutz <noah.jutz@gmail.com>
2020-05-17 20:58:41 +0200

    Add ranger to auto-install

--- .scripts/auto-install.sh
+++ .scripts/auto-install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo pacman -Syy
-sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher htop virtualbox
+sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher htop virtualbox ranger
 yay -S android-studio bitwarden
