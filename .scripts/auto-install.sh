ab9e3d99f1c9eb80074b2918465e764eb50e2e47
noahjutz <noah.jutz@gmail.com>
2020-05-17 13:53:24 +0200

    Add virtualbox to auto-install

--- .scripts/auto-install.sh
+++ .scripts/auto-install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo pacman -Syy
-sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher htop
+sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher htop virtualbox
 yay -S android-studio bitwarden
