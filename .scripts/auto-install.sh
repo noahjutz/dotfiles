9a0acc8b7304c137c083f88bfe73c7cfe596bfb8
noahjutz <noah.jutz@gmail.com>
2020-05-17 22:04:44 +0200

    Configure the git bare repository in auto-install
    Instead of having the user execute the command, it's now in auto-install

--- .scripts/auto-install.sh
+++ .scripts/auto-install.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+
+# Initial setup #
+
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
+
+# Install packages #
+
 sudo pacman -Syy
 sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher htop virtualbox ranger
 yay -S android-studio bitwarden
+
+# Setup dependent on packages #
+
 dconf load / < .backup/gnome-settings
