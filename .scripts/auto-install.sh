4c598be96cded1256d0ae28affaacd4654fc577e
noahjutz <noah.jutz@gmail.com>
2020-05-17 21:38:43 +0200

    Load desktop env settings in auto-install

--- .scripts/auto-install.sh
+++ .scripts/auto-install.sh
@@ -2,3 +2,4 @@
 sudo pacman -Syy
 sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher htop virtualbox ranger
 yay -S android-studio bitwarden
+dconf load / < .backup/gnome-settings
