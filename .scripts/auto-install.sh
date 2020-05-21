aab6b164ce265cd26e1ee6e8e7ec38e2a0748a82
noahjutz <noah.jutz@gmail.com>
2020-05-21 13:49:31 +0200

    Add brave to auto-install

--- .scripts/auto-install.sh
+++ .scripts/auto-install.sh
@@ -8,7 +8,7 @@ echo ".cfg" >> .gitignore
 # Install packages #
 
 sudo pacman -Syy
-sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher htop virtualbox ranger etcher
+sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher htop virtualbox ranger etcher brave
 yay -S android-studio bitwarden
 
 # Restore backup #
