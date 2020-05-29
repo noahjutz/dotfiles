a332bc24a531aee1a3d16e3fdf8285ba285cf6c7
noahjutz <noah.jutz@gmail.com>
2020-05-29 13:44:13 +0200

    Install according to prompts

--- .scripts/install/auto-install.sh
+++ .scripts/install/auto-install.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-# parameters
-case $1 in
-	"-f" )
-		echo FULL
-		;;
-	"-m" )
-		echo MINIMAL
-		;;
-	* )
-		cat "help"
-		exit
-		;;
-esac
-
-if [[ $1 == "-m" ]]; then
-	read -p ":: Config repo? [Y/n]: "	inp_cfg_repo
-	read -p ":: Restore backup? [Y/n]: "	inp_restore
-	read -p ":: Config shell? [Y/n]: "	inp_cfg_sh
-	read -p ":: Install packages? [Y/n]: "	inp_install_pkg
-	read -p ":: Execute? [Y/n]: "		inp_execute
-	echo $inp_cfg_repo, $inp_restore, $inp_cfg_sh, $inp_install_pkg, $inp_execute
-fi
-
 config_repo() {
 	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 	echo ".cfg" >> .gitignore
@@ -40,3 +17,37 @@ install_packages() {
 	sh ~/.scripts/install-packages.sh
 }
 
+# parameters
+case $1 in
+	"-f" )
+		echo FULL
+		;;
+	"-m" )
+		echo MINIMAL
+		;;
+	* )
+		cat "help"
+		exit
+		;;
+esac
+
+if [[ $1 == "-m" ]]; then
+	echo Select what to do. Default is Y.
+	read -p ":: Config repo? [Y/n]: "	inp_cfg_repo
+	read -p ":: Restore backup? [Y/n]: "	inp_restore
+	read -p ":: Config shell? [Y/n]: "	inp_cfg_sh
+	read -p ":: Install packages? [Y/n]: "	inp_install_pkg
+	read -p ":: Execute? [Y/n]: "		inp_execute
+fi
+
+if [[ $inp_execute == [Nn] ]]; then
+	echo Canceled.
+	exit
+else
+	echo Start auto-install.
+fi
+
+if [[ $inp_cfg_repo != [Nn] ]]; then config_repo; fi
+if [[ $inp_restore != [Nn] ]]; then restore_backup; fi
+if [[ $inp_cfg_sh != [Nn] ]]; then config_shell; fi
+if [[ $inp_install_pkg != [Nn] ]]; then install_packages; fi
