a5690eb58ac8a1372d416d807a7a0f15430558f4
noahjutz <noah.jutz@gmail.com>
2020-05-29 12:52:39 +0200

    Wrap installation in functions

--- .scripts/install/auto-install.sh
+++ .scripts/install/auto-install.sh
@@ -14,16 +14,20 @@ case $1 in
 		;;
 esac
 
-# git
-/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
-echo ".cfg" >> .gitignore
+config_git() {
+	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
+	echo ".cfg" >> .gitignore
+}
 
-# restore backup
-sh ~/.scripts/restore.sh
+restore_backup() {
+	sh ~/.scripts/restore.sh
+}
 
-# change shell to fish
-chsh -s /bin/fish
+config_shell() {
+	chsh -s /bin/fish
+}
 
-# packages
-sh ~/.scripts/install-packages.sh
+install_packages() {
+	sh ~/.scripts/install-packages.sh
+}
 
