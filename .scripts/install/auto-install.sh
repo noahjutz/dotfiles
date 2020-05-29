48f555245295a462111ebd5962740a12ffd135a9
noahjutz <noah.jutz@gmail.com>
2020-05-29 13:05:17 +0200

    Add prompts to minimal auto-install

--- .scripts/install/auto-install.sh
+++ .scripts/install/auto-install.sh
@@ -14,7 +14,16 @@ case $1 in
 		;;
 esac
 
-config_git() {
+if [[ $1 == "-m" ]]; then
+	read -p ":: Config repo? [Y/n]: "	inp_cfg_repo
+	read -p ":: Restore backup? [Y/n]: "	inp_restore
+	read -p ":: Config shell? [Y/n]: "	inp_cfg_sh
+	read -p ":: Install packages? [Y/n]: "	inp_install_pkg
+	read -p ":: Execute? [Y/n]: "		inp_execute
+	echo $inp_cfg_repo, $inp_restore, $inp_cfg_sh, $inp_install_pkg, $inp_execute
+fi
+
+config_repo() {
 	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 	echo ".cfg" >> .gitignore
 }
