b3a638927c8f9a6c0d8dcee0aba4807572d26f6a
noahjutz <noah.jutz@gmail.com>
2020-05-29 13:56:31 +0200

    Move install-packages.sh to auto-install.sh

--- .scripts/install/auto-install.sh
+++ .scripts/install/auto-install.sh
@@ -14,7 +14,9 @@ config_shell() {
 }
 
 install_packages() {
-	sh ~/.scripts/install-packages.sh
+	sudo pacman -Syy
+	sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher htop virtualbox ranger etcher brave wipe gimp gvim
+	yay -S android-studio bitwarden
 }
 
 # parameters
--- .scripts/install/install-packages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo pacman -Syy
-sudo pacman -S --noconfirm fish yay code discord tor torbrowser-launcher htop virtualbox ranger etcher brave wipe gimp gvim
-yay -S android-studio bitwarden
