49bcb75ff835869be3045a40422d77ed2b0d6e2a
noahjutz <noah.jutz@gmail.com>
2020-05-31 12:49:42 +0200

    Add brave backup

--- .scripts/backup.sh
+++ .scripts/backup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Gnome
+echo Gnome ...
 dconf dump / > ~/.backup/gnome-settings
+echo Gnome DONE
 
 # Brave
+echo Brave ...
 tar czf ~/.backup/brave.tar.gz ~/.config/BraveSoftware
+echo Brave DONE
